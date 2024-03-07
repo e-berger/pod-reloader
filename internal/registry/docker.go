13a4ae823dc2ab96332a7c793c9e86592a7e8c0f
damien-jacinto <damien.jacinto@finalcad.com>
2024-03-07 21:41:14 +0100

    add version docker

--- internal/registry/docker.go
+++ internal/registry/docker.go
@@ -46,7 +46,7 @@ func (d *RegistryDocker) String() string {
 
 func (d *RegistryDocker) RetreiveImage(i *imageref.ImageRef) (string, error) {
 	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion("1.24"))
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion("1.30"))
 	if err != nil {
 		return "", err
 	}
